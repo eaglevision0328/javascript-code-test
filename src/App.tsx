3bf4c7626f9795f267b8faab5d42fb4bd1ff4db2
eaglevision0328 <eaglevision0328@gmail.com>
2025-02-28 13:53:58 +0000

    Initial TEST APP commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Edf energy Test</h1>
+      
+    </div>
+  );
+}
+
+export default App;
